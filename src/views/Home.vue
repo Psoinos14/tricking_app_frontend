9a1605cddcab5570684f898a09e6fd3c659ca11b
shane <smp1993@gmail.com>
2019-08-26 22:59:09 -0400

    made it so user can only see friends statuses

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <button v-on:click="createStatus()">What have you been up to?</button>
+    <input type="text" v-model="newStatusText" placeholder="Land a new trick?"/> 
+    <button v-on:click="createFriends()">Post Status</button>
     <h2>{{message2}}</h2>
-    <div v-for="status in statuses">
-      <h5>Tricker: {{status.user}} </h5>
+    <div v-for="friend in friends">
+      <h5>Tricker: {{friend.user}} </h5>
+      <div v-for="status in friend.statuses">
       <p> {{status.text}} </p>
       <p> ------------------------------------------------------- </p>
+    </div>
     </div>
   </div>
 </template>
@@ -22,23 +25,32 @@ export default {
     return {
       message: "Welcome to TrickTogether!",
       message2: "See what other trickers have been up to:",
-      statuses: [],
+      friends: [],
+      newStatusText: "",
+
+      errors: [],
     };
   },
   created: function() {
     axios.get("/api/statuses").then(response => {
-      this.statuses = response.data;
+      console.log(response.data);
+      this.friends = response.data;
     });
   },
   methods: {
-    createStatus: function() {
-      var newStatus = {
-        Text: this.newText,
-        User: this.currentuser
-        // Day: this.newDayAndTimeDay,
-        // Time: this.newDayAndTimeTime,
-        // Gym: this.newDayAndTimeGymID,
+    createFriends: function() {
+      var params = {
+        text: this.newStatusText
       };
+      axios
+        .post("/api/statuses", params)
+        .then(response => {
+          
+        })
+        .catch(error => {
+          console.log(error.response);
+          this.errors = error.response.data.errors;
+        });
     }
   }
 };
