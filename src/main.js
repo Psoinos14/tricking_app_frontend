66faed55ad9a47288b14a11e0ac98512eeb4c6e4
shane <smp1993@gmail.com>
2019-12-22 20:13:50 -0500

    Revert "changed url to heroku url in main js"
    This reverts commit dd622ab165706f728b1e4df6531c2d3ca8066a9d.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import axios from "axios";
 
 axios.defaults.baseURL = 
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://tricking-back.herokuapp.com";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
